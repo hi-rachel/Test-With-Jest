07e0c2955ec7bae7c87fa54aceb6dea9d7fb67a0
hi-rachel <rachel.uiux@gmail.com>
2025-07-08 16:05:39 +0900

    feat: 3주차 과제 완료
    - Section 2 완료

--- src/ch2/models/index.spec.js
+++ src/ch2/models/index.spec.js
@@ -1,20 +1,24 @@
+// 테스트 환경에서는 NODE_ENV가 설정되어 있지 않음
+// 테스트 커버리지 100%을 위한
+// delete process.env.NODE_ENV;
+
 beforeEach(() => {
   jest.resetModules();
 });
 
-it('models/index.js이 잘 실행된다', () => {
-  const db = require('./index');
-  jest.spyOn(db.Hashtag, 'initiate').mockImplementation();
-  jest.spyOn(db.Post, 'initiate').mockImplementation();
-  jest.spyOn(db.User, 'initiate').mockImplementation();
+it("models/index.js이 잘 실행된다", () => {
+  const db = require("./index");
+  jest.spyOn(db.Hashtag, "initiate").mockImplementation();
+  jest.spyOn(db.Post, "initiate").mockImplementation();
+  jest.spyOn(db.User, "initiate").mockImplementation();
   expect(db).toBeDefined();
-})
+});
 
-it('models/index.js이 NODE_ENV가 undefined일 때도 잘 실행된다', () => {
+it("models/index.js이 NODE_ENV가 undefined일 때도 잘 실행된다", () => {
   delete process.env.NODE_ENV;
-  const db = require('./index');
-  jest.spyOn(db.Hashtag, 'initiate').mockImplementation();
-  jest.spyOn(db.Post, 'initiate').mockImplementation();
-  jest.spyOn(db.User, 'initiate').mockImplementation();
+  const db = require("./index");
+  jest.spyOn(db.Hashtag, "initiate").mockImplementation();
+  jest.spyOn(db.Post, "initiate").mockImplementation();
+  jest.spyOn(db.User, "initiate").mockImplementation();
   expect(db).toBeDefined();
-})
+});
